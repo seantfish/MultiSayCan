59023d59a1a10a72b1602b76ac1b001eee1f0e62
Sean Fish <seantfish@gmail.com>
2024-04-21 03:29:45 -0400

    SayCan WIP

--- plan_d/Webots-reinforcement-navigation-master/controllers/PPO/PPO.py
+++ plan_d/Webots-reinforcement-navigation-master/controllers/PPO/PPO.py
@@ -19,7 +19,15 @@ import numpy as np
 
 from math import atan2, pi
 import torch
+from PIL import Image
+
+# from PIL import Image
+
+import requests
+from transformers import AutoProcessor, AutoModelForCausalLM
 import openai
+
+from heapq import nlargest
 
 
 
@@ -137,6 +145,10 @@ class GroundRobot(RobotSupervisorEnv):
         """
         normalized_value = (value - min_value) / (max_value - min_value)        
         return normalized_value
+    
+    def get_camera_view(self, camera_name):
+        camera = self.getDevice(camera_name)
+        return camera.getImageArray()
     
     def get_distance_to_goal(self):
         """
@@ -303,11 +315,11 @@ class GroundRobot(RobotSupervisorEnv):
             return True
         return False
 
-    # def stop(self):
-    #     self.left_motor.setPosition(0)
-    #     self.right_motor.setPosition(0)
-    #     self.left_motor.setVelocity(0)
-    #     self.right_motor.setVelocity(0)
+    def stop(self):
+        self.left_motor.setPosition(0)
+        self.right_motor.setPosition(0)
+        self.left_motor.setVelocity(0)
+        self.right_motor.setVelocity(0)
     
     def get_info(self):
         info = {
@@ -409,11 +421,29 @@ class Action():
         return affordance
     
 class CameraAction():
-   def __init__(self, camera, env):
-      self.env = env
-      self.camera = camera
+    def __init__(self, camera, env):
+        self.env = env
+        self.camera = camera
     
+    def go(self):
+        image = self.env.get_camera_view(self.camera)
+        return np.array(image)
+    
+    def get_affordance(self):
+        return 1
+    
+class TerminationAction():
+    def __init__(self, env, max_val=1):
+      self.env = env
+      self.max_val = max_val
 
+    def go(self):
+        self.env.stop()
+   
+    def get_affordance(self):
+        return -1
+
+# Sample Action Sets
 
 action_set = {
     'go to the red square': Action([-2, -2], env, model),
@@ -423,16 +453,77 @@ action_set = {
 }
 
 camera_action_set = {
-    'get an overhead view': CameraAction('uav camera', env)
+    'get a description of the robot camera view': CameraAction('camera', env),
+    'get a description of an overhead overhead view': CameraAction('uav camera', env)
+}
+
+termination_action_set = {
+    'done': TerminationAction(env)
+}
+
+# Experimental Action Sets
+
+base_action_set = {
+    'go to the red square': Action([-2, -2], env, model),
+    'go to the blue square': Action([2, -2], env, model),
+    'go to the green square': Action([2, 2], env, model),
+    'go to the yellow square': Action([-2, 2], env, model),
+    'done': TerminationAction(env)
 }
 
 # Sanity Check
 \
-# for action in action_set.keys():
-#     print(action)
-#     do_action = action_set[action]
-#     print(do_action.get_affordance())
-#     do_action.go()
+
+img = None
+for action in camera_action_set.keys():
+    print(action)
+    do_action = camera_action_set[action]
+    print(do_action.get_affordance())
+    img = do_action.go()
+    # plt.imshow(do_action.go())
+    # plt.show()
+
+# =================================================================================================================
+# GIT CAPTIONING
+# =================================================================================================================
+cache_dir = "C:/Users/Sean/Projects/Classes/CS8803DLM/MultiSayCan/plan_d/Webots-reinforcement-navigation-master/controllers/PPO/model_cache"
+model_dir = cache_dir + "/coco/snapshots/a"
+from pathlib import Path
+
+# path = Path(str_path)
+model_path = Path(model_dir)
+
+# git_processor = AutoProcessor.from_pretrained("microsoft/git-base-coco", cache_dir=cache_dir, local_files_only=True)
+# git_model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco", cache_dir=cache_dir, local_files_only=True)
+git_processor = AutoProcessor.from_pretrained(model_path, local_files_only=True)
+git_model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True)
+
+questions = [
+   "the square colors are ",
+   "furniture in the room includes "
+]
+
+def generate_caption(image, processor, model, question=""):
+    # url = "http://images.cocodataset.org/val2017/000000039769.jpg" #640x480
+    # image = Image.open(requests.get(url, stream=True).raw)
+    # image 
+
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values
+
+    # question = "what colors are the squares? red, yellow, green, or blue?"
+
+    input_ids = processor(text=question, add_special_tokens=False).input_ids
+    input_ids = [processor.tokenizer.cls_token_id] + input_ids
+    input_ids = torch.tensor(input_ids).unsqueeze(0)
+
+    # generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
+    generated_ids = model.generate(pixel_values=pixel_values, input_ids=input_ids, max_length=50)
+    generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    # generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)
+    print(generated_caption)
+    return generated_caption
+
+generate_caption(img, git_processor, git_model)
 
 # =================================================================================================================
 # SayCan
@@ -447,7 +538,8 @@ overwrite_cache = True
 if overwrite_cache:
   LLM_CACHE = {}
 
-def gpt3_call(engine="text-ada-001", prompt="", max_tokens=128, temperature=0,
+
+def gpt3_call(engine="itdontmatter", prompt="", max_tokens=128, temperature=0,
               logprobs=1, echo=False):
   full_query = ""
   for p in prompt:
@@ -477,7 +569,7 @@ def gpt3_call(engine="text-ada-001", prompt="", max_tokens=128, temperature=0,
     LLM_CACHE[id] = response
   return response
 
-def gpt3_scoring(query, options, engine="text-ada-001", limit_num_options=None, option_start="\n", verbose=False, print_tokens=False):
+def gpt3_scoring(query, options, engine="itdontmatter", limit_num_options=None, option_start="\n", verbose=False, print_tokens=False):
   if limit_num_options:
     options = options[:limit_num_options]
   verbose and print("Scoring", len(options), "options")
@@ -512,24 +604,108 @@ def gpt3_scoring(query, options, engine="text-ada-001", limit_num_options=None, 
 
   return scores, response
 
-# def make_options(pick_targets=None, place_targets=None, options_in_api_form=True, termination_string="done()"):
-#   if not pick_targets:
-#     pick_targets = PICK_TARGETS
-#   if not place_targets:
-#     place_targets = PLACE_TARGETS
-#   options = []
-#   for pick in pick_targets:
-#     for place in place_targets:
-#       if options_in_api_form:
-#         option = "robot.pick_and_place({}, {})".format(pick, place)
-#       else:
-#         option = "Pick the {} and place it on the {}.".format(pick, place)
-#       options.append(option)
+# =================================================================================================================
 
-#   options.append(termination_string)
-#   print("Considering", len(options), "options")
-#   return options
+def build_scene_description(found_objects, block_name="box", bowl_name="circle"):
+  scene_description = f"objects = {found_objects}"
+  scene_description = scene_description.replace(block_name, "block")
+  scene_description = scene_description.replace(bowl_name, "bowl")
+  scene_description = scene_description.replace("'", "")
+  return scene_description
 
+def step_to_nlp(step):
+  step = step.replace("robot.pick_and_place(", "")
+  step = step.replace(")", "")
+  pick, place = step.split(", ")
+  return "Pick the " + pick + " and place it on the " + place + "."
+
+def normalize_scores(scores):
+  max_score = max(scores.values())
+  normed_scores = {key: np.clip(scores[key] / max_score, 0, 1) for key in scores}
+  return normed_scores
+
+def plot_saycan(llm_scores, vfs, combined_scores, task, correct=True, show_top=None):
+  if show_top:
+    top_options = nlargest(show_top, combined_scores, key = combined_scores.get)
+    # add a few top llm options in if not already shown
+    top_llm_options = nlargest(show_top // 2, llm_scores, key = llm_scores.get)
+    for llm_option in top_llm_options:
+      if not llm_option in top_options:
+        top_options.append(llm_option)
+    llm_scores = {option: llm_scores[option] for option in top_options}
+    vfs = {option: vfs[option] for option in top_options}
+    combined_scores = {option: combined_scores[option] for option in top_options}
+
+  sorted_keys = dict(sorted(combined_scores.items()))
+  keys = [key for key in sorted_keys]
+  positions = np.arange(len(combined_scores.items()))
+  width = 0.3
+
+  fig = plt.figure(figsize=(12, 6))
+  ax1 = fig.add_subplot(1,1,1)
+
+  plot_llm_scores = normalize_scores({key: np.exp(llm_scores[key]) for key in sorted_keys})
+  plot_llm_scores = np.asarray([plot_llm_scores[key] for key in sorted_keys])
+  plot_affordance_scores = np.asarray([vfs[key] for key in sorted_keys])
+  plot_combined_scores = np.asarray([combined_scores[key] for key in sorted_keys])
+
+  ax1.bar(positions, plot_combined_scores, 3 * width, alpha=0.6, color="#93CE8E", label="combined")
+
+  score_colors = ["#ea9999ff" for score in plot_affordance_scores]
+  ax1.bar(positions + width / 2, 0 * plot_combined_scores, width, color="#ea9999ff", label="vfs")
+  ax1.bar(positions + width / 2, 0 * plot_combined_scores, width, color="#a4c2f4ff", label="language")
+  ax1.bar(positions - width / 2, np.abs(plot_affordance_scores), width, color=score_colors)
+
+  plt.xticks(rotation="vertical")
+  ax1.set_ylim(0.0, 1.0)
+
+  ax1.grid(True, which="both")
+  ax1.axis("on")
+
+  ax1_llm = ax1.twinx()
+  ax1_llm.bar(positions + width / 2, plot_llm_scores, width, color="#a4c2f4ff", label="language")
+  ax1_llm.set_ylim(0.01, 1.0)
+  plt.yscale("log")
+
+  font = {"fontname":"Arial", "size":"16", "color":"k" if correct else "r"}
+  plt.title(task, **font)
+  key_strings = [key.replace("robot.pick_and_place", "").replace(", ", " to ").replace("(", "").replace(")","") for key in keys]
+  plt.xticks(positions, key_strings, **font)
+  ax1.legend()
+  plt.show()
+
+
+
+def affordance_scoring(actions):
+    affordance_scores = {}
+    for action_desc in actions.keys():
+        action = actions[action_desc]
+        action_aff = action.get_affordance()
+        if action_desc != 'done':
+            affordance_scores[action_desc] = action_aff
+    max_val = np.max(np.array(list(affordance_scores.values())))
+    affordance_scores['done'] = .2 * max_val
+    return affordance_scores
+
+
+# query = "To move to the red square, I should:\n"
+
+# options = base_action_set.keys()
+# llm_scores, _ = gpt3_scoring(query, options, verbose=True)
+
+# affordance_scores = affordance_scoring(base_action_set)
+
+# combined_scores = {option: np.exp(llm_scores[option]) * affordance_scores[option] for option in options}
+# combined_scores = normalize_scores(combined_scores)
+# selected_task = max(combined_scores, key=combined_scores.get)
+# print("Selecting: ", selected_task)
+
+tasks = [
+   "visit four different color squares",
+   "find a chair",
+   "find a chair near a green square",
+   "find a table near a red square"
+]
 
 
 
